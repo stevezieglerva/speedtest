2078796a92786ec5a5c94e5317ab39acf298b46c
Steve Ziegler <stephen.v.ziegler@gmail.com>
2020-06-20 19:05:43 -0400

    remove loop and track location

--- run-speedtest.sh
+++ run-speedtest.sh
@@ -1,8 +1,11 @@
 
 
-while true
-do 
+#while true
+#do 
+    echo "Where are you?"
+    read location
     echo "*** $(date)" > wifi.txt
+    echo "Location: $location" >> wifi.txt
     networksetup -getairportnetwork en0 >> wifi.txt
     cat wifi.txt
     cat wifi.txt >> log.txt
@@ -11,8 +14,8 @@ do
     cat results.txt
     cat results.txt >> log.txt
 
-    sleep 15m
-done
+#    sleep 15m
+#done
 
 
 
